62d942453680eede9e81539959fa7e1b9245de2e
Antonio de la Torre <antonio@codesai.com>
2017-11-16 19:27:21 +0100

    When second operand is zero then result is first operand.

--- src/Fractions.js
+++ src/Fractions.js
@@ -1,2 +1,17 @@
 'use strict';
 
+function Fraction(numerator, denominator) {
+    return {
+        add: function (other) {
+            return Fraction(numerator, denominator);
+        },
+
+        numerator: function () {
+            return numerator;
+        },
+
+        denominator: function () {
+            return denominator;
+        }
+    };
+}
--- test/FractionsTest.js
+++ test/FractionsTest.js
@@ -1,7 +1,13 @@
 'use strict';
 
-describe('Fractions Should', function() {
-    it('test', function() {
-        expect(1).toBe(1);
+describe('Adding fractions', function() {
+    it('when second operand is zero the result is the first operand', function() {
+        var fractionA = Fraction(1, 1);
+        var fractionB = Fraction(0, 1);
+
+        var result = fractionA.add(fractionB);
+
+        expect(result.numerator()).toBe(1);
+        expect(result.denominator()).toBe(1);
     });
 });
